888241dc2ccc7421d5753c5aa198e7c6142eaf4c
trunghoai123 <trunghoaiitiuh@gmail.com>
2022-11-16 18:39:31 +0700

    gateway api

---
+++ app-ban-sach-api-gateway/src/main/java/com/example/demo/AppBanSachApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBanSachApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBanSachApiGatewayApplication.class, args);
+	}
+
+}
